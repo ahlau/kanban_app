78116c85d7374aa64390d12028500d67acb8c8da
Alfred Lau <alfred@betatypelabs.com>
2016-04-22 11:58:26 -0400

    made it prettier with CSS

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -29,7 +29,7 @@ export default class Note extends React.Component {
     const onDelete = this.props.onDelete;
     return(
       <div onClick={this.edit}>
-        <span>{this.props.task}</span>
+        <span className="task">{this.props.task}</span>
         {onDelete ? this.renderDelete() : null}
       </div>
       );
@@ -57,6 +57,6 @@ export default class Note extends React.Component {
   };
 
   renderDelete = () => {
-    return <button onClick={this.props.onDelete}>X</button>
+    return <button className="delete-note" onClick={this.props.onDelete}>X</button>
   };
 }
